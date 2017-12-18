845ba284bc1da0ab18fc8fd3abbf5e379f6a7c3a
Johanna Gomez <pao_gomez1@hotmail.com>
2017-12-18 14:06:58 -0600

    bonus included

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,60 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    private int hitCount;
+
+    public void init(){
+        // Reset hit counter.
+        hitCount = 0;
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+
+//        res.getWriter().println("Hello, world");
+
+        // Set response content type
+        res.setContentType("text/html");
+
+
+        PrintWriter out = res.getWriter();
+
+
+        String name = req.getParameter("name");
+
+        if (name == null){
+            out.println("<h1>Hello, World!</h1>");
+        } else {
+            out.println("<h1>Hello, "+ name + "</h1>");
+        }
+
+
+
+
+//        out.println("<input type=\"text\" size=\"20\" name=\"txtUserName\" />");
+
+
+//      This method executes whenever the servlet is hit
+//      increment hitCount
+
+        hitCount++;
+        String title = "Total Number visits on this page";
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<h2 align = \"center\">" + hitCount + "</h2>\n" +
+                "</html>"
+      );
 
     }
 }
